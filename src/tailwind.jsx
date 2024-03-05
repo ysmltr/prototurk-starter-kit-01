b69b0992505bc611d5300faf57b4e475cc32d790
ysmltr <yesimltr1@gmail.com>
2024-03-05 15:18:00 +0300

    commit

---
+++ src/tailwind.jsx
@@ -0,0 +1,11 @@
+function Tailwind() {
+    return(
+    <div className="bg-red-500 py-8 my-9">
+                <h1 className="hover:bg-blue-500 hover:italic bg-orange-600">Hello tailwind</h1>
+
+    </div>
+    )
+}
+
+
+export default Tailwind;
